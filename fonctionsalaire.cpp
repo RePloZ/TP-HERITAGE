751b102e1b3f6d1fb35bda85ecb3e36f5a89ab47
RePloZ <herve.kay@ynov.com>
2017-01-30 21:06:23 +0100

    V 1.0

--- fonctionsalaire.cpp
+++ fonctionsalaire.cpp
@@ -14,24 +14,32 @@ int Interimaires::Salaire_Technicien_interimaires(int h_travail){
 }
 //Représentant
 Representant::Representant() : m_primeRepresentants(250);
-  int Representant::Salaire_Commerciaux_Representant(m_primeRepresentants){
+  int Representant::Salaire_Commerciaux_Representant(int m_primeRepresentants){
   int deplacement(0);
-  cout << Combien de déplacement avez vous fait << endl;
+  int prime(0);
+  cout << "Combien de déplacement avez vous fait ?" << endl;
   cin >> deplacement;
-  salaire = m_primeRepresentants + (deplacement*50);
+  cout << "Combien de fois avez-vous obtenu une prime ?"
+  cin >> prime;
+  salaire = 2500 + (m_primeRepresentants*prime) + (deplacement*50);
   return salaire;
 }
 //salariés
+Salaries::Salaroes();
 int Salaries::Salaire_technicien_salaries(int salaire){
-
+return salaire
 }
 //Vendeur
 Vendeurs::Vendeurs() : m_primeVendeurs();
-Vendeurs::
-
+Vendeurs::Salaire_Commerciaux_Vendeurs(int m_primeVendeurs){
+int prime;
+cout << Combien avez vous eu de prime << endl;
+cin >> prime;
+salaire = m_primeVendeurs + (prime*)
+}
 void afficherletout(string nom; string firstName;string age;int salaire){
   cout << "Nom :" << nom << endl;
   cout << "Prénom :" << firstName << endl;
   cout << "Âge :" << age << endl;
-    cout << "Salaire :" << /*Récuperer la valeur salaire*/ << endl;
+  cout << "Salaire :" << /*Récuperer la valeur salaire*/ << endl;
 }
