46ca230ca4ee6019d17b189e140b51a84dde38eb
VonBabibel <VonBabibel@users.noreply.github.com>
2017-01-30 15:33:56 +0100

    Add files via upload

---
+++ salaries.h
@@ -0,0 +1,13 @@
+#ifndef SALARIES
+#define SALARIES
+#include "technicien.h"
+
+class Salaries : public Technicien {
+	public: 
+		Salaries();
+		int Salaire_technicien_salaries();
+	private:
+		int salaire(2000);
+};
+
+#endif
