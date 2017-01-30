36f1410cd82a8a840a7002316ac04184fb8c10eb
RePloZ <herve.kay@ynov.com>
2017-01-30 21:20:04 +0100

    V 1.0
    Correction

--- main.cpp
+++ main.cpp
@@ -3,7 +3,46 @@
 #include "technicien.h"
 #include "representants.h"
 #include "salarie.h"
+
 using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string name;
+  string firstName;
+  string age;
+  int proffession;
+  cout << Quel est votre nom : << endl;
+  cin >> name;
+  cout << Quel est votre prénom : << endl;
+  cin >> firstName;
+  cout << Quel est votre âge : << endl;
+  cin >> age;
+  cout << Votre proffession << endl;
+  cout << "1 - Interimaire" << endl;
+  cout << "2 - Representants" << endl;
+  cout << "3 - Technicien" << endl;
+  cout << "4 - Vendeurs" << endl;
+  cin >> proffession;
+  switch (proffession) {
+    case 1 :
+int Interimaires::Salaire_Technicien_interimaires(int h_travail);
+void afficherletout(nom; firstName; age; salaire);
+      break;
+    case 2 :
+    int Representant::Salaire_Commerciaux_Representant(int m_primeRepresentants);
+    void afficherletout(nom; firstName; age; salaire);
+      break;
+    case 3 :
+    int Salaries::Salaire_technicien_salaries();
+    void afficherletout(nom; firstName; age; salaire);
+      break;
+    case 4 :
+Vendeurs::Salaire_Commerciaux_Vendeurs(int m_primeVendeurs);
+void afficherletout(nom; firstName; age; salaire);
+      break;
+  }
+  return 0;
+}
 
 //Interimaires
 Interimaires::Interimaires() : h_travail(300);
@@ -25,7 +64,7 @@ Representant::Representant() : m_primeRepresentants(250);
   return salaire;
 }
 //salariés
-Salaries::Salaroes();
+Salaries::Salaires();
 int Salaries::Salaire_technicien_salaries(int salaire){
 return salaire
 }
