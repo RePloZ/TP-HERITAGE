3ce12ef692d4bd6becec2ad73f42a7153a20dc11
RePloZ <herve.kay@ynov.com>
2017-01-30 15:59:45 +0100

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "interimaire.h"
+#include "technicien.h"
+#include "representants.h"
+#include "salarie.h"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string name;
+  string firstName;
+  string age;
+  int proffession;
+  cout << Quel est votre nom : << endl;
+  cin >> name;
+  cout << Quel est votre prénom : << endl;
+  cin >> firstName;
+  cout << Quel est votre âge : << endl;
+  cin >> age;
+  cout << Votre proffession << endl;
+  cout << "1 - Interimaire" << endl;
+  cout << "2 - Technicien" << endl;
+  cout << "3 - Representants" << endl;
+  cout << "4 - Salarie" << endl;
+  switch (proffession) {
+    case 1 :
+    
+      break;
+    case 2 :
+
+      break;
+    case 3 :
+
+      break;
+    case 4 :
+
+      break;
+  }
+  return 0;
+}
