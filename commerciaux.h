00804eb6c0d80a972e0b2cd59cad1943d49465f8
AndryRama <andry.ramanana@ynov.com>
2017-01-30 14:52:50 +0100

    Update commerciaux.h

--- commerciaux.h
+++ commerciaux.h
@@ -2,4 +2,13 @@
 #define COMMERCIAUX
 #include "employes.h"
 
-class Commerciaux : public Employes {}
+class Commerciaux : public Employes {
+
+  int getSalaireBase();
+  void setSalaireBase (int salaireBase);
+
+  private:
+  std::int m_salaireBase;
+
+};
+
