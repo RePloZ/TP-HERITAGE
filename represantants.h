9a799609c9e6d232471ff40324a6e70b11840f25
mountakha <mountakha.djigo@ynov.com>
2017-01-30 14:50:36 +0100

    Update represantants.h

--- represantants.h
+++ represantants.h
@@ -5,3 +5,22 @@ en publique
 fonction pour le salaire
 fonction pour afficher les valeurs
 */
+#ifndef DEF_REPRESENTANT
+#define DEF_REPRESENTANT
+ 
+#include <iostream>
+#include <string>
+ 
+class représentant
+{
+    public:
+        représentant();
+        void getrepresentant(int prime);
+        void setrepresentant(string representant) const;
+ 
+    private:
+        int m_prime;
+        std::string m_representant;
+};
+ 
+#endif
